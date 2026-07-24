e9dd7ea9e081a9e8f372fb6d383b8df62f32734c
csv610 <csv610@yahoo.com>
2026-07-24 12:36:55 +0530

    Introduce modern CMakeLists.txt C++23 build configuration and fix chapter1 forwarding conflict

--- src/chapter1/random_utils.h
+++ src/chapter1/random_utils.h
@@ -1,53 +1,7 @@
 #pragma once
-
-#include <random>
-#include <vector>
-#include <algorithm>
-#include <chrono>
+#include "../../include/ral/random_utils.h"
 
 namespace chapter1 {
-
-class RandomEngine {
-public:
-    static RandomEngine& instance() {
-        static RandomEngine eng;
-        return eng;
-    }
-    
-    std::mt19937& engine() { return engine_; }
-    
-    int rand_int(int min, int max) {
-        std::uniform_int_distribution<int> dist(min, max);
-        return dist(engine_);
-    }
-    
-    double rand_double(double min = 0.0, double max = 1.0) {
-        std::uniform_real_distribution<double> dist(min, max);
-        return dist(engine_);
-    }
-    
-    bool coin_flip(double p = 0.5) {
-        return rand_double() < p;
-    }
-    
-    template<typename T>
-    void shuffle(std::vector<T>& vec) {
-        std::shuffle(vec.begin(), vec.end(), engine_);
-    }
-    
-    // Geometric distribution - number of trials until first success
-    int geometric(double p) {
-        std::geometric_distribution<int> dist(p);
-        return dist(engine_);
-    }
-
-private:
-    RandomEngine() : engine_(std::chrono::steady_clock::now().time_since_epoch().count()) {}
-    std::mt19937 engine_;
-};
-
-inline RandomEngine& rng() {
-    return RandomEngine::instance();
+using randalgo::rng;
+using randalgo::RandomEngine;
 }
-
-} // namespace chapter1
